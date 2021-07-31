8c04c3377df2232b209f212804fddc3baf2ef70b
HenryGalvez <soulofhenry@gmail.com>
2021-07-31 11:42:05 -0600

    ejemplo1

---
+++ Ejemplo1/main.py
@@ -0,0 +1,15 @@
+from Aritmetica import *
+
+a = Aritmetica()
+
+print(a.suma(1,2))
+print(a.resta(2,2))
+print(a.multiplicacion(5,5))
+
+from Relacional import *
+
+r = Relacional()
+
+print(r.mayor(1,2))
+print(r.menor(2,2))
+print(r.igual(5,5))
