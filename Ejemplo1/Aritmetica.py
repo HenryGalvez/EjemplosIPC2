5f7ee7a900100b4d46e56e80f66c0ed5977bcce8
HenryGalvez <soulofhenry@gmail.com>
2021-07-31 11:39:58 -0600

    startup

---
+++ Ejemplo1/Aritmetica.py
@@ -0,0 +1,9 @@
+class Aritmetica:
+    def suma(self, x, y):
+        return 2*x+y
+    
+    def resta(self, x, y):
+        return x-y
+    
+    def multiplicacion(self, x, y):
+        return x*y
